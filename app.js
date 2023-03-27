a550dedd1e6bf8e0aa499f50803ac85e284056ee
Fredy Velasquez <macacoloco696@gmail.com>
2023-03-27 08:21:47 -0600

    finished app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 
-//dynamic route
+//dynamic route - creo un patron que va a ser seguido para todas las preguntas
 app.get('/questions/:id/:title', (req, res) => {
     res.send('this route handles all the questions')
 });
 
-
+//route en donde accedo a los params
 app.get('/users/:id/:username', (req, res) => {
+    let userId = req.params.id;
     let user = req.params.username;
-
-    res.send('This route will handle ' + user + ' profile')
+    res.send('This route will handle ' + user + ' profile that has id ' + userId)
 })
 
 app.listen(3000, ()=> {
