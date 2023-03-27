3e53d1422c8629817da00935b5ec0e86676d4d29
Fredy Velasquez <macacoloco696@gmail.com>
2023-03-27 08:14:11 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+//dynamic route
+app.get('/questions/:id/:title', (req, res) => {
+    res.send('this route handles all the questions')
+});
+
+app.get('/users/:id/:username', (req, res) => {
+    res.send('This route will handle all the users of stackoverflow.')
+})
+
+app.listen(3000, ()=> {
+    console.log('listen on 3000')
+})
